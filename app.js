d835368bb7ba5cad1e7ca41074b454f916cd4b02
mydreamworldstudio <martin.yhm@gmail.com>
2025-03-17 17:04:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ async function startPrediction() {
 async function connectMicrobit() {
     try {
         microbitDevice = await navigator.bluetooth.requestDevice({
-            acceptAllDevices: true,
+            filters: [{ namePrefix: "BBC micro:bit" }], // Only show micro:bit devices
             optionalServices: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e'] // UART Service UUID
         });
 
